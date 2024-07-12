8347ab734651d441a0b84bcd6ca97e8e673574e4
Serhii Tarasenko <stdev33@gmail.com>
2024-07-12 19:14:20 +0300

    task 1

--- task_1.py
+++ task_1.py
@@ -1,16 +1,34 @@
-from datetime import datetime
+import pathlib
+
+current_dir = pathlib.Path(__file__).parent
 
 
-def get_days_from_today(date):
+def total_salary(path):
     try:
-        input_date = datetime.strptime(date, "%Y-%m-%d")
-        today = datetime.today()
-        delta = today - input_date
-        return delta.days
-    except ValueError:
-        return "Invalid date format. Please, use format 'YYYY-MM-DD'."
+        with open(path, 'r', encoding='utf-8') as file:
+            salaries = []
+            for line in file:
+                name, salary = line.strip().split(',')
+                salaries.append(float(salary))
 
+        if not salaries:
+            return (0, 0)
 
-print(get_days_from_today("2020-10-09"))
-print(get_days_from_today("2024-07-08"))
-print(get_days_from_today("test_invalid_date_format"))
+        total = sum(salaries)
+        average = total / len(salaries)
+        return (total, average)
+
+    except FileNotFoundError:
+        print(f"Файл за шляхом {path} не знайдено.")
+        return (0, 0)
+    except Exception as e:
+        print(f"Сталася помилка: {e}")
+        return (0, 0)
+
+def main():
+    path_to_file = current_dir / "salary_file.txt"
+    total, average = total_salary(path_to_file)
+    print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+
+if __name__ == "__main__":
+    main()
